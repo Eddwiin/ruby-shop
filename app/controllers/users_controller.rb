a1f10ce134617cf0ef335211affabb98fafc88e3
Eddwiin <maguran.mahendrarajan@gmail.com>
2023-01-08 23:53:59 +0100

    add update and delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,27 @@
 class UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:create]
-  
+  skip_before_action :verify_authenticity_token, only: [:create, :update, :destroy]
+  before_action :set_user, only: [:update, :destroy]
+
   def create
     @user = User.new(user_params)
     if @user.save 
       render json: true, status: :created 
     else 
-      debugger
       render json: { errors: @user.errors.full_messages}, status: :unprocessable_entity
     end
   end
 
   def update
+    if @user.update(user_params)
+      render json: true, status: :ok 
+    else 
+      render json: { errors: @user.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
-  def delete
+  def destroy
+    @user.delete
+    render json: true, status: :ok 
   end
 
   private 
@@ -22,4 +29,8 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:last_name, :first_name, :email, :phone, :adress, :password)
   end
+
+  def set_user
+    @user = User.find(params[:user][:id])
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/create'
-  get 'users/update'
-  get 'users/delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   
-  resources :users, only: [:create, :update, :delete]
+  resources :users, only: [:create, :update, :destroy]
 end
